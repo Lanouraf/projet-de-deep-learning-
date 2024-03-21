5cf5e192f8639379ad62a9abd3d18cc630773771
maudjacquot <156662948+maudjacquot@users.noreply.github.com>
2024-03-21 18:02:17 +0100

    Update DATALOAD1.py
    ajout de data_apple

--- DATALOAD1.py
+++ DATALOAD1.py
@@ -15,3 +15,21 @@ def data_twitter():
     directory = "apple-twitter-sentiment-texts.csv"
     home_data = pd.read_csv(directory)
     return home_data
+
+def data_apple():
+    """Loads the Apple quality data from Kaggle.
+
+    Returns
+    -------
+    df: pd.DataFrame
+        apple quality dataset to do classification tasks.
+
+    Notes
+    -----
+    This is the dataset dowloaded from
+        "https://www.kaggle.com/datasets/nelgiriyewithana/apple-quality"
+
+    """
+    directory = "apple_quality.csv"
+    home_data = pd.read_csv(directory)
+    return home_data
