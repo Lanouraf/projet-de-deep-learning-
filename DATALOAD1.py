36125fc3d44894800ccc56509638d9d07a4c205d
maudjacquot <156662948+maudjacquot@users.noreply.github.com>
2024-03-21 18:00:31 +0100

    Create DATALOAD1.py

---
+++ DATALOAD1.py
@@ -0,0 +1,17 @@
+def data_twitter():
+    """Loads the Apple Twitter sentiment texts data from Kaggle.
+
+    Returns
+    -------
+    df: pd.DataFrame
+        The Apple Twitter sentiment texts data.
+
+    Notes
+    -----
+    This is the dataset dowloaded from
+        "https://www.kaggle.com/datasets/seriousran/appletwittersentimenttexts/download?datasetVersionNumber=1"
+
+    """
+    directory = "apple-twitter-sentiment-texts.csv"
+    home_data = pd.read_csv(directory)
+    return home_data
