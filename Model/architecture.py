41e2d13d1481dc82231fac7dee9355ac8f12465d
thomas <lanouraf@gmail.com>
2024-03-21 18:35:50 +0100

    commit

--- Model/architecture.py
+++ Model/architecture.py
@@ -1,1 +1,104 @@
 #fichier où on met les différentes architectures torch de nos modèles 
+
+
+import torch.nn as nn
+
+from BatchNorm import BatchNorm2D, BatchNorm1D
+
+
+class LeNet(nn.Module):
+    """
+    LeNet-5 architecture
+    """
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size = 5, padding = 2),  # C1: 28x28x6
+            nn.AvgPool2d(kernel_size = 2, stride = 2),  # S2: 14x14x6
+            nn.Sigmoid(),
+
+            nn.Conv2d(6, 16, kernel_size = 5),  # C3: 10x10x16
+            nn.AvgPool2d(kernel_size = 2, stride = 2),  # 5x5x16
+            nn.Sigmoid(),
+
+            nn.Flatten(),
+            nn.Linear(5 * 5 * 16, 120),
+            nn.Sigmoid(),
+
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
+
+            nn.Linear(84, 10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class LeNetBN(nn.Module):
+    """
+    LeNet-5 architecture with BatchNorm blocks
+    """
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size = 5, padding = 2),  # C1: 28x28x6
+            nn.AvgPool2d(kernel_size = 2, stride = 2),  # S2: 14x14x6
+            BatchNorm2D(6),
+            nn.Sigmoid(),
+
+            nn.Conv2d(6, 16, kernel_size = 5),  # C3: 10x10x16
+            nn.AvgPool2d(kernel_size = 2, stride = 2),  # 5x5x16
+            BatchNorm2D(16),
+            nn.Sigmoid(),
+
+            nn.Flatten(),
+            nn.Linear(5 * 5 * 16, 120),
+            BatchNorm1D(120),
+            nn.Sigmoid(),
+
+            nn.Linear(120, 84),
+            BatchNorm1D(84),
+            nn.Sigmoid(),
+
+            nn.Linear(84, 10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class LeNetStockBN(nn.Module):
+    """
+    LeNet-5 architecture with Pytorch BatchNorm blocks
+    """
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size = 5, padding = 2),  # C1: 28x28x6
+            nn.AvgPool2d(kernel_size = 2, stride = 2),  # S2: 14x14x6
+            nn.BatchNorm2d(6),
+            nn.Sigmoid(),
+
+            nn.Conv2d(6, 16, kernel_size = 5),  # C3: 10x10x16
+            nn.AvgPool2d(kernel_size = 2, stride = 2),  # 5x5x16
+            nn.BatchNorm2d(16),
+            nn.Sigmoid(),
+
+            nn.Flatten(),
+            nn.Linear(5 * 5 * 16, 120),
+            nn.BatchNorm1d(120),
+            nn.Sigmoid(),
+
+            nn.Linear(120, 84),
+            nn.BatchNorm1d(84),
+            nn.Sigmoid(),
+
+            nn.Linear(84, 10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
---
+++ init.py
@@ -0,0 +1,21 @@
+from .BatchNorm import BatchNorm
+
+
+def BatchNorm2D(filters, epsilon=1e-5):
+    """
+    2D version of BatchNorm block
+    :param filters: Number of input filters
+    :param epsilon: Constant for numerical stability
+    :return: BatchNorm block module
+    """
+    return BatchNorm(filters, epsilon, dims='2D')
+
+
+def BatchNorm1D(filters, epsilon=1e-5):
+    """
+    1D version of BatchNorm block
+    :param filters: Number of input filters
+    :param epsilon: Constant for numerical stability
+    :return: BatchNorm block module
+    """
+    return BatchNorm(filters, epsilon, dims='1D')
