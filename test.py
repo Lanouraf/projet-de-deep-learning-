9622c2a1c4c89cb187e37da4e620fc81f38a1499
thomas <lanouraf@gmail.com>
2024-03-22 18:17:20 +0100

    ajout du saving de modèles pour batchnorm

--- test.py
+++ test.py
@@ -5,9 +5,8 @@ The trained models include Vanilla LeNet-5, LeNet-5 with Batch Normalization, an
 The loss values are saved in separate numpy files for each model configuration.
 """
 
-
-
 # Rest of the code...
+import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -66,52 +65,97 @@ for i in range(len(file_names)):
         pass
 # Train models
 # ---------------------------------------------
+
+min_val_loss = float('inf')  # Initialize minimum validation loss as infinity
 # Vanilla LeNet-5
 all_losses_vanilla = []
 all_acc_vanilla = []
+
 for i in range(3):
     model_vanilla = LeNet()
-    losses_vanilla, val_losses_vanilla, acc_vanilla = fit(model_vanilla, train_dataloader, valid_dataloader,
+    losses_vanilla, val_losses_vanilla, acc_vanilla ,model_vanilla = fit(model_vanilla, train_dataloader, valid_dataloader,
                                                           epochs=10, lr=1e-3)
     all_losses_vanilla.append((losses_vanilla, val_losses_vanilla))
     all_acc_vanilla.append(acc_vanilla)
+     
+    # Check if the current model has a lower validation loss
+    if min(val_losses_vanilla) < min_val_loss:
+        min_val_loss = min(val_losses_vanilla)
+        # Save best Vanilla LeNet-5 model
+        best_model_vanilla = model_vanilla
+        torch.save(best_model_vanilla.state_dict(), 'best_model_vanilla.pth')
 
 # BatchNorm LeNet-5
 all_losses_bn = []
 all_acc_bn = []
 for i in range(3):
     model_bn = LeNetBN()
-    losses_bn, val_losses_bn, acc_bn = fit(model_bn, train_dataloader, valid_dataloader, epochs=10, lr=1e-3)
+    losses_bn, val_losses_bn, acc_bn ,model_bn= fit(model_bn, train_dataloader, valid_dataloader, epochs=10, lr=1e-3)
     all_losses_bn.append((losses_bn, val_losses_bn))
     all_acc_bn.append(acc_bn)
+    
+    # Check if the current model has a lower validation loss
+    if min(val_losses_bn) < min_val_loss:
+        min_val_loss = min(val_losses_bn)
+        # Save best Vanilla LeNet-5 model
+        best_model_bn = model_bn
+        torch.save(best_model_bn.state_dict(), 'best_model_bn.pth')
 
 # BatchNorm LeNet-5 with high lr
 all_losses_bn2 = []
 for i in range(3):
     model_bn2 = LeNetBN()
-    losses_bn2, val_losses_bn2, _ = fit(model_bn2, train_dataloader, valid_dataloader, epochs=10, lr=1e-2)
+    losses_bn2, val_losses_bn2, _,model_bn2 = fit(model_bn2, train_dataloader, valid_dataloader, epochs=10, lr=1e-2)
     all_losses_bn2.append((losses_bn2, val_losses_bn2))
+    
+        # Check if the current model has a lower validation loss
+    if min(val_losses_bn2) < min_val_loss:
+        min_val_loss = min(val_losses_bn2)
+        # Save best Vanilla LeNet-5 model
+        best_model_bn2 = model_bn2
+        torch.save(best_model_bn2.state_dict(), 'best_model_bn2.pth')
 
 # BatchNorm LeNet-5 with even higher lr
 all_losses_bn3 = []
 for i in range(3):
     model_bn3 = LeNetBN()
-    losses_bn3, val_losses_bn3, _ = fit(model_bn3, train_dataloader, valid_dataloader, epochs=10, lr=5e-2)
+    losses_bn3, val_losses_bn3, _ ,model_bn3= fit(model_bn3, train_dataloader, valid_dataloader, epochs=10, lr=5e-2)
     all_losses_bn3.append((losses_bn3, val_losses_bn3))
+    
+            # Check if the current model has a lower validation loss
+    if min(val_losses_bn3) < min_val_loss:
+        min_val_loss = min(val_losses_bn3)
+        # Save best Vanilla LeNet-5 model
+        best_model_bn3 = model_bn3
+        torch.save(best_model_bn2.state_dict(), 'best_model_bn3.pth')
 
 # Vanilla LeNet-5 with even higher lr
 all_losses_vanilla2 = []
 for i in range(3):
     model_vanilla2 = LeNet()
-    losses_vanilla2, val_losses_vanilla2, _ = fit(model_vanilla2, train_dataloader, valid_dataloader, epochs=10, lr=5e-2)
+    losses_vanilla2, val_losses_vanilla2, _ ,model_vanilla2= fit(model_vanilla2, train_dataloader, valid_dataloader, epochs=10, lr=5e-2)
     all_losses_vanilla2.append((losses_vanilla2, val_losses_vanilla2))
+    
+                # Check if the current model has a lower validation loss
+    if min(val_losses_vanilla2) < min_val_loss:
+        min_val_loss = min(val_losses_vanilla2)
+        # Save best Vanilla LeNet-5 model
+        best_model_vanilla2 = model_vanilla2
+        torch.save(best_model_vanilla2.state_dict(), 'best_model_vanilla2.pth')
 
 # BatchNorm LeNet-5 using Pytorch's BatchNorm
 all_losses_stockbn = []
 for i in range(3):
     model_stockbn = LeNetStockBN()
-    losses_stockbn, val_losses_stockbn, _ = fit(model_stockbn, train_dataloader, valid_dataloader, epochs=10, lr=1e-3)
+    losses_stockbn, val_losses_stockbn, _ ,model_stockbn= fit(model_stockbn, train_dataloader, valid_dataloader, epochs=10, lr=1e-3)
     all_losses_stockbn.append((losses_stockbn, val_losses_stockbn))
+    
+                    # Check if the current model has a lower validation loss
+    if min(val_losses_stockbn) < min_val_loss:
+        min_val_loss = min(val_losses_stockbn)
+        # Save best Vanilla LeNet-5 model
+        best_model_stockbn = model_stockbn
+        torch.save(best_model_stockbn.state_dict(), 'best_model_stockbn.pth')
 
 
 
@@ -136,4 +180,6 @@ np.save('losses_vanilla.npy', losses_vanilla)
 
 # Plot results
 # ---------------------------------------------
+
+
 print(all_losses_bn3)
