f5c62efbc0b7d17b4b3b4e5e22811eb89edd04c4
thomas <lanouraf@gmail.com>
2024-03-23 20:39:19 +0100

    copie des losses clean sur app2

--- Homemade_Batchnorm.py
+++ Homemade_Batchnorm.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from google_drive_downloader import GoogleDriveDownloader as gdd
 import torch
+import os
 
 def homemade_batchnormalisation():
     st.title("Homemade Batch Normalisation")
@@ -16,27 +17,30 @@ def homemade_batchnormalisation():
     st.write("- Un modèle LeNet-5  avec la BatchNorm de Pytorch")
     st.write("- Un modèle LeNet-5 avec notre BatchNorm fait main")
     
+    # Instructions sur les fichiers de loss     
+    # Téléchargement des fichiers de loss de nos modèles déjà entrainés depuis Google Drive
     
-        
-# Téléchargement des fichiers de loss de nos modèles déjà entrainés depuis Google Drive
-   
- # Liste des IDs de vos fichiers sur Google Drive
-    file_ids = ['11IH2ZXJ3b_tZezDk8kN3Doar2-cs5YIZ', '1HEeypE2pBz7KpogT0KW4eQNTEp7hJhSd', '1ENI0CFZjgyM9A2rW_tIg6_nAXs6531oS','1YVzklFpo5ty6ApDK-XBzdb18zuogkQsY']
+    # Vérifier si les fichiers de loss existent déjà
+    if not os.path.exists('valeur loss-models-batch//losses_vanilla.npy') or not os.path.exists('valeur loss-models-batch//losses_vanilla.npy') or not os.path.exists('valeur loss-models-batch//losses_vanilla.npy') or not os.path.exists('valeur loss-models-batch//losses_vanilla.npy'):
+       # Liste des IDs de vos fichiers sur Google Drive
+        file_ids = ['11IH2ZXJ3b_tZezDk8kN3Doar2-cs5YIZ', '1HEeypE2pBz7KpogT0KW4eQNTEp7hJhSd', '1ENI0CFZjgyM9A2rW_tIg6_nAXs6531oS','1YVzklFpo5ty6ApDK-XBzdb18zuogkQsY']
 
- # Liste des noms de fichiers de sortie
-    output_filenames = ['losses_vanilla.npy', 'losses_bn.npy', 'val_losses_vanilla.npy','val_losses_bn.npy']
+        # Liste des noms de fichiers de sortie
+        output_filenames = ['losses_vanilla.npy', 'losses_bn.npy', 'val_losses_vanilla.npy','val_losses_bn.npy']
 
- # Boucle pour télécharger chaque fichier de loss depuis Google Drive
-    for file_id, output_filename in zip(file_ids, output_filenames):
-     url = f'https://drive.google.com/file/d/{file_id}/view?usp=sharing'
-     dest_path = './' + output_filename
-     gdd.download_file_from_google_drive(file_id=file_id, dest_path=dest_path, overwrite=True, showsize=True)
+        # Boucle pour télécharger chaque fichier de loss depuis Google Drive
+        for file_id, output_filename in zip(file_ids, output_filenames):
+            url = f'https://drive.google.com/file/d/{file_id}/view?usp=sharing'
+            dest_path = './valeur loss-models-batch/' + output_filename
+            gdd.download_file_from_google_drive(file_id=file_id, dest_path=dest_path, overwrite=True, showsize=True)
+    else:
+        st.write("Les fichiers de loss existent déjà.")
 
 # Charger les fichiers de loss
-    losses_vanilla = np.load('losses_vanilla.npy')
-    losses_bn = np.load('losses_bn.npy')
-    val_losses_vanilla = np.load('val_losses_vanilla.npy')
-    val_losses_bn = np.load('val_losses_bn.npy')
+    losses_vanilla = np.load('valeur loss-models-batch//losses_vanilla.npy')
+    losses_bn = np.load('valeur loss-models-batch//losses_bn.npy')
+    val_losses_vanilla = np.load('valeur loss-models-batch//val_losses_vanilla.npy')
+    val_losses_bn = np.load('valeur loss-models-batch//val_losses_bn.npy')
 
     st.write(val_losses_bn)
     if losses_vanilla is not None and losses_bn is not None and val_losses_vanilla is not None and val_losses_bn is not None:
